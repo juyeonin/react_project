22a8ee6338265e4290bebfcb0be42677d49effaa
juyeonin <ijy912@naver.com>
2022-02-17 17:52:46 +0900

    commit

---
+++ src/lib/dummy.ts
@@ -0,0 +1,13 @@
+const tags: {
+  tagId: string;
+  tag: string;
+}[] = [
+  { tagId: '1', tag: '전자제품' },
+  { tagId: '2', tag: '아이템' },
+  { tagId: '3', tag: '과일' },
+  { tagId: '4', tag: '테스트' },
+];
+
+export default {
+  tags,
+};
